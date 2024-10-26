3bbe25137cb6103244aa89a1bbaa57e91c1f1ac9
PierreAnders <pierre.untas@gmail.com>
2024-10-26 23:51:03 +0200

    Initial Commit

---
+++ PowerHealth/PowerHealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  PowerHealthApp.swift
+//  PowerHealth
+//
+//  Created by Pierre Untas on 26/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct PowerHealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PowerHealthTests/PowerHealthTests.swift
@@ -0,0 +1,17 @@
+//
+//  PowerHealthTests.swift
+//  PowerHealthTests
+//
+//  Created by Pierre Untas on 26/10/2024.
+//
+
+import Testing
+@testable import PowerHealth
+
+struct PowerHealthTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
